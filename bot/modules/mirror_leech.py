8f17467d9b79d8a185ed6fffd3f1c35072dd7c3b
nhnobnd1 <89453030+nhnobnd1@users.noreply.github.com>
2023-03-04 01:20:34 +0700

    asdasdasd

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -19,7 +19,8 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import sendMessage
 from bot.helper.listener import MirrorLeechListener
-
+import json
+from requests import get as rget
 
 @new_task
 async def _mirror_leech(client, message, isZip=False, extract=False, isQbit=False, isLeech=False, sameDir={}):
