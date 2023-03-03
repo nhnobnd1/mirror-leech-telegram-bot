9d3e4f39870dbc4c816cef5df2f9acbeaed96c66
nhnobnd1 <89453030+nhnobnd1@users.noreply.github.com>
2023-03-04 01:17:40 +0700

    asd

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -19,8 +19,6 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import sendMessage
 from bot.helper.listener import MirrorLeechListener
-import json
-from requests import get as rget
 
 
 @new_task
@@ -121,14 +119,6 @@ async def _mirror_leech(client, message, isZip=False, extract=False, isQbit=Fals
     if link != '':
         link = re_split(r"pswd:|\|", link)[0]
         link = link.strip()
-    LOGGER.info(f'data message {message_args[1]}')
-    # if ",j" in message_args[1]:
-        
-    #     dataTorrent = rget(f'https://magnetread.onrender.com/special/?date={message_args[1]}')
-    #     LOGGER.info(f'data response{json.loads(dataTorrent.content)}')
-    #     torrentLink = json.loads(dataTorrent.content)
-    #     for torrent in torrentLink:
-    #         await sendMessage(message,torrent)
 
     if reply_to := message.reply_to_message:
         file_ = reply_to.document or reply_to.photo or reply_to.video or reply_to.audio or \
