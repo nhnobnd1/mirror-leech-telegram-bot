e1d5475884f19ee402412bec62748e586340bb8c
nhnobnd1 <89453030+nhnobnd1@users.noreply.github.com>
2023-04-01 18:15:47 +0700

    33

--- bot/helper/mirror_utils/download_utils/telegram_downloader.py
+++ bot/helper/mirror_utils/download_utils/telegram_downloader.py
@@ -96,6 +96,7 @@ class TelegramDownloadHelper:
             message = await user.get_messages(chat_id=message.chat.id, message_ids=message.id)
         media = message.document or message.photo or message.video or message.audio or \
                  message.voice or message.video_note or message.sticker or message.animation or None
+        LOGGER.info(f'hehe chuarn chua {path}')
         if media is not None:
             async with global_lock:
                 download = media.file_unique_id not in GLOBAL_GID
