2867113dcc42b03038072f3a25a9601799b6d25c
nhnobnd1 <89453030+nhnobnd1@users.noreply.github.com>
2023-04-02 21:34:45 +0700

    asdasd

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -141,7 +141,7 @@ async def _mirror_leech(client, message, isZip=False, extract=False, isQbit=Fals
             await sendMessage(message,torrent)
     if "name=" in message_args[1]:
         parts=message_args[1].split("=")
-        
+        #test cai jenkins
         if len(parts)==2:
             dataTorrent = rget(f'{URL_MAGNET}?name={parts[1]}')
             LOGGER.info(f'{URL_MAGNET}?name={parts[1]}')
